9ae8fdafa8909b25c4f94cd15d89b9036c28e74d
shishupei <shishupei10@163.com>
2024-04-28 17:46:53 +0800

    清洗 gitee comment事件

--- common/pushSdk/HuaweiCloudMsgConfig.go
+++ common/pushSdk/HuaweiCloudMsgConfig.go
@@ -10,12 +10,5 @@ type HWCloudMsgConfig struct {
 }
 
 func NewTestConfig() HWCloudMsgConfig {
-	return HWCloudMsgConfig{
-		AppInfoKey:    "C5I7EH4iPr4xPXRNa65Qg70bATxP",
-		AppInfoSecret: "slpTMyXRJdAOldFm2bw6fxdtpoQy",
-		ApiAddress:    "https://smsapi.cn-south-1.myhuaweicloud.com:443/sms/batchSendSms/v1",
-		Sender:        "8824041123653",
-		TemplateId:    "33fabc75615647a886a87daa7064f766",
-		Signature:     "开源社区",
-	}
+	return HWCloudMsgConfig{}
 }
