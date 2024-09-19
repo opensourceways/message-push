5bdf42d80d92a98bdc69b3f15bf271128e66e0f7
shishupei <shishupei10@163.com>
2024-09-19 14:45:27 +0800

    读取推送配置，分发消息

---
+++ service/handler.go
@@ -0,0 +1,169 @@
+package service
+
+import (
+	"time"
+
+	"github.com/goccy/go-json"
+	"github.com/gocql/gocql"
+	"github.com/opensourceways/message-push/common/cassandra"
+	"github.com/opensourceways/message-push/common/pushSdk"
+	"github.com/opensourceways/message-push/config"
+	"github.com/opensourceways/message-push/models/bo"
+	"github.com/opensourceways/message-push/models/dto"
+	"github.com/sirupsen/logrus"
+	"github.com/todocoder/go-stream/stream"
+)
+
+func GiteeHandle(payload []byte, _ map[string]string) error {
+	event := dto.NewCloudEvents()
+	msgBodyErr := json.Unmarshal(payload, &event)
+	if msgBodyErr != nil {
+		return msgBodyErr
+	}
+	res := handle(event, config.GiteeConfigInstance.Push)
+	return res
+}
+
+func EurBuildHandle(payload []byte, _ map[string]string) error {
+	event := dto.NewCloudEvents()
+	msgBodyErr := json.Unmarshal(payload, &event)
+	if msgBodyErr != nil {
+		return msgBodyErr
+	}
+	res := handle(event, config.EurBuildConfigInstance.Push)
+	return res
+}
+
+func OpenEulerMeetingHandle(payload []byte, _ map[string]string) error {
+	event := dto.NewCloudEvents()
+	msgBodyErr := json.Unmarshal(payload, &event)
+	if msgBodyErr != nil {
+		return msgBodyErr
+	}
+	res := handle(event, config.MeetingConfigInstance.Push)
+	return res
+}
+
+func CVEHandle(payload []byte, _ map[string]string) error {
+	event := dto.NewCloudEvents()
+	msgBodyErr := json.Unmarshal(payload, &event)
+	if msgBodyErr != nil {
+		return msgBodyErr
+	}
+	res := handle(event, config.CVEConfigInstance.Push)
+	return res
+}
+
+func handle(event dto.CloudEvents, push config.PushConfig) error {
+	raw := make(dto.Raw)
+	raw.FromJson(event.Data())
+	recipients := event.GetRecipient()
+	if recipients == nil || len(recipients) == 0 {
+		return nil
+	}
+	flatRaw := raw.Flatten()
+	stream.Of(recipients...).ForEach(
+		func(item bo.RecipientPushConfig) {
+			handleInnerMessage(event, flatRaw, item)
+			isFilter := flatRaw.ModeFilter(item.ModeFilter)
+			if isFilter {
+				handleMessage(event, raw, flatRaw, item, push)
+				handleMail(event, flatRaw, item, push)
+			}
+		},
+	)
+	return nil
+}
+
+func handleInnerMessage(event dto.CloudEvents, flatRaw dto.FlatRaw, pushConfig bo.RecipientPushConfig) {
+	res := sendInnerMessage(event, pushConfig)
+	if res.Res == dto.Failed {
+		logrus.Info("send inner message ", event.ID()+" failed")
+	} else {
+		logrus.Info("send inner message ", event.ID()+" success")
+	}
+	insertData(event, flatRaw, res)
+}
+
+func handleMessage(event dto.CloudEvents, raw dto.Raw, flatRaw dto.FlatRaw, pushConfig bo.RecipientPushConfig, push config.PushConfig) {
+	if pushConfig.NeedMessage {
+		res := sendHWCloudMessage(raw, pushConfig, push.MsgConfig)
+		if res.Res == dto.Failed {
+			logrus.Info("send message ", event.ID()+" failed", pushConfig.Message)
+		} else {
+			logrus.Info("send message ", event.ID()+" success", pushConfig.Message)
+		}
+		insertData(event, flatRaw, res)
+	}
+}
+
+func handleMail(event dto.CloudEvents, flatRaw dto.FlatRaw, pushConfig bo.RecipientPushConfig, push config.PushConfig) {
+	if pushConfig.NeedMail {
+		res := sendMail(event, pushConfig, push.EmailConfig)
+		if res.Res == dto.Failed {
+			logrus.Info("send mail ", event.ID()+" failed", pushConfig.Mail)
+		} else {
+			logrus.Info("send mail ", event.ID()+" success", pushConfig.Mail)
+		}
+		//insertData(event, flatRaw, res)
+		logrus.Info("send mail ", event.ID()+" success，接收人", pushConfig.Mail)
+	}
+}
+
+func sendHWCloudMessage(raw dto.Raw, recipient bo.RecipientPushConfig, messageConfig pushSdk.MsgConfig) dto.PushResult {
+	templateParas := raw.ToMessageArgs(recipient.MessageTemplate)
+	res := pushSdk.SendHWCloudMessage(messageConfig, templateParas, recipient)
+	if res.Res == dto.Failed {
+		logrus.Error("send hwcloud message failed", templateParas)
+	}
+	return res
+}
+
+func sendInnerMessage(event dto.CloudEvents, recipient bo.RecipientPushConfig) dto.PushResult {
+	return event.SendInnerMessage(recipient)
+}
+
+func sendMail(event dto.CloudEvents, recipient bo.RecipientPushConfig, emailConfig pushSdk.EmailConfig) dto.PushResult {
+	return pushSdk.SendEmail(event.Extensions()["mailTitle"].(string),
+		event.Extensions()["mailSummary"].(string), recipient, emailConfig)
+}
+
+func insertData(eurBuildEvent dto.CloudEvents, flatRaw dto.FlatRaw, result dto.PushResult) {
+	stringifyMap := flatRaw.StringifyMap()
+	insert := `insert into message_push_record (recipient_id, time_uuid, created_at, event_data, event_data_content_type,
+                                 event_data_schema, event_id, event_source, event_source_url, event_spec_version,
+                                 event_time, event_type, event_user, push_address, push_state, push_time, push_type,
+                                 remark, title, summary)
+values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);
+`
+	err := cassandra.Session().
+		Query(
+			insert,
+			result.RecipientId,
+			gocql.TimeUUID(),
+			time.Now(),
+			stringifyMap,
+			eurBuildEvent.DataContentType(),
+			eurBuildEvent.DataSchema(),
+			eurBuildEvent.ID(),
+			eurBuildEvent.Source(),
+			eurBuildEvent.Extensions()["sourceurl"].(string),
+			eurBuildEvent.SpecVersion(),
+			eurBuildEvent.Time(),
+			eurBuildEvent.Type(),
+			eurBuildEvent.Extensions()["user"].(string),
+			result.PushAddress,
+			result.Res,
+			result.Time,
+			result.PushType,
+			result.Remark,
+			eurBuildEvent.Extensions()["title"].(string),
+			eurBuildEvent.Extensions()["summary"].(string),
+		).
+		Exec()
+	if err != nil {
+		logrus.Errorf("insert data failed, err:%v", err)
+		panic(nil)
+		return
+	}
+}
---
+++ service/subscribe.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	kfklib "github.com/opensourceways/kafka-lib/agent"
+	"github.com/opensourceways/message-push/config"
+	"github.com/sirupsen/logrus"
+)
+
+func SubscribeEurEvent() {
+	logrus.Info("subscribing to eur topic")
+	_ = kfklib.Subscribe(config.EurBuildConfigInstance.Kafka.Group, EurBuildHandle, []string{config.EurBuildConfigInstance.Kafka.Topic})
+}
+
+func SubscribeGiteeEvent() {
+	logrus.Info("subscribing to gitee topic")
+	_ = kfklib.Subscribe(config.GiteeConfigInstance.Kafka.Issue.Group, GiteeHandle, []string{config.GiteeConfigInstance.Kafka.Issue.Topic, config.GiteeConfigInstance.Kafka.Push.Topic, config.GiteeConfigInstance.Kafka.PR.Topic, config.GiteeConfigInstance.Kafka.Note.Topic})
+}
+
+func SubscribeMeetingEvent() {
+	logrus.Info("subscribing to meeting topic")
+	_ = kfklib.Subscribe(config.MeetingConfigInstance.Kafka.Group, OpenEulerMeetingHandle, []string{config.MeetingConfigInstance.Kafka.Topic})
+}
+
+func SubscribeCVEEvent() {
+	logrus.Info("subscribing to cve topic")
+	_ = kfklib.Subscribe(config.CVEConfigInstance.Kafka.Group, CVEHandle, []string{config.CVEConfigInstance.Kafka.Topic})
+}
