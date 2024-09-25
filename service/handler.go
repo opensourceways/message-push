37a86e49cf2000051d1e0d016c18cb2eb38f63ea
毛毛 <5427081+maomao19970922@user.noreply.gitee.com>
2024-09-25 09:02:42 +0800

    del debug

--- service/handler.go
+++ service/handler.go
@@ -66,7 +66,6 @@ func handle(event dto.CloudEvents, push config.PushConfig) error {
 		func(item bo.RecipientPushConfig) {
 			handleInnerMessage(event, flatRaw, item)
 			isFilter := flatRaw.ModeFilter(item.ModeFilter)
-			logrus.Infof("the filter is %v %v, email is %v,", isFilter, item.ModeFilter, item.Mail)
 			if isFilter {
 				handleMessage(event, raw, flatRaw, item, push)
 				handleMail(event, flatRaw, item, push)
