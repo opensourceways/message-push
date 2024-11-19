87827462d3623cb9364fd3497446e479f742c250
shishupei <shishupei10@163.com>
2024-11-19 17:21:25 +0800

    端点id从secrets获取

--- .github/script/codearts_check.sh
+++ .github/script/codearts_check.sh
@@ -43,7 +43,7 @@ DATA=$(cat <<EOF
       "git_type" : "github",
       "default_branch" : "feture_experence",
       "git_url" : "https://github.com/opensourceways/message-push.git",
-      "endpoint_id" : $5,
+      "endpoint_id" : "$5",
       "build_params" : {
         "build_type" : "branch",
         "event_type" : "Manual",
