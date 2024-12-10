cba0f3d7ed8a716807c67cfaa821a243bb1560c5
Hourunze1997 <1043170898@qq.com>
2024-12-10 14:58:07 +0800

    test

--- config/meetingConfig.go
+++ config/meetingConfig.go
@@ -16,9 +16,11 @@ type MeetingConfig struct {
 
 func InitMeetingConfig(configFile string) {
 	cfg := new(MeetingConfig)
+	logrus.Infof("configFile is %v", configFile)
 	if err := utils.LoadFromYaml(configFile, cfg); err != nil {
 		logrus.Error("Config初始化失败, err:", err)
 		return
 	}
+	logrus.Infof("the cfg is %v", cfg)
 	MeetingConfigInstance = *cfg
 }
